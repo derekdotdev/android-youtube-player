2d5ad639b59379f5b256aaa5babaed04141bb6d7
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-08-03 12:00:37 -0400

    Use overloaded createVideoIntent and createPlaylistIntent methods to enable auto play functionality on launch.

--- app/src/main/java/com/derekdileo/youtubeplayer/StandaloneActivity.java
+++ app/src/main/java/com/derekdileo/youtubeplayer/StandaloneActivity.java
@@ -46,11 +46,11 @@ public class StandaloneActivity extends AppCompatActivity implements View.OnClic
 
         switch(view.getId()) {
             case R.id.btnPlayVideo:
-                intent = YouTubeStandalonePlayer.createVideoIntent(this, YouTubeActivity.GOOGLE_API_KEY, YouTubeActivity.YOUTUBE_VIDEO_ID);
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YouTubeActivity.GOOGLE_API_KEY, YouTubeActivity.YOUTUBE_VIDEO_ID, 0, true, true);
                 break;
 
             case R.id.btnPlayList:
-                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, YouTubeActivity.GOOGLE_API_KEY, YouTubeActivity.YOUTUBE_PLAYLIST);
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, YouTubeActivity.GOOGLE_API_KEY, YouTubeActivity.YOUTUBE_PLAYLIST, 0, 0, true, true);
                 break;
 
             default:
